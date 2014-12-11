b248e87a6bc5ed5e790d2268c94a682c0de675a1
Pravin Parkhi <pravinp@egnaroinc.com>
2014-12-12 02:15:25 +0530

    Initial cheking of sales tool.

---
+++ Demo/src/com/demo/Main.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData data = new SalesData();
+		
+		displsyGreeting();
+		data.display();
+	}
+
+	private static void displsyGreeting() {
+
+		System.out.println("Hello Happy sales data people");
+		System.out.println("This application shows salesdata");
+	}
+}
